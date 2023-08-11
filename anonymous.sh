33687482e8ac26f67cfef5db22af775e1e19167c
vibhor123vib <142025480+vibhor123vib@users.noreply.github.com>
2023-08-11 19:57:13 +0530

    Add files via upload

---
+++ anonymous.sh
@@ -0,0 +1,24 @@
+#this is made for all termux and kali users
+#hi i am vibhor
+#subscribe our channel of YouTube
+#Youtube channel link https://youtube.com/@AnonymousHackers95
+#so lets start code 
+
+
+apt update
+apt upgrade
+apt install git wget curl openssl
+apt install proxychains-ng
+git clone https://github.com/vib123vib/anonymous
+cd
+rm $PREFIX/etc/proxychains.conf
+cd
+cd anonymous
+ls
+cd anonymousfiles
+cp proxychains.conf $PREFIX/etc
+cd
+apt install tor
+tor
+#open new session and enjoy anonymity
+
